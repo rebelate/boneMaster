cc19dd0cb06ad4477ec2221b9d7d637e5ad62b26
Juan Gea <juangea@bone-studio.com>
2020-02-16 10:30:00 +0100

    Lightgroups integration on new Object.cpp refactor, separated in tiny files

--- intern/cycles/blender/addon/properties.py
+++ intern/cycles/blender/addon/properties.py
@@ -25,6 +25,7 @@ from bpy.props import (
     IntProperty,
     PointerProperty,
     StringProperty,
+    CollectionProperty,
 )
 
 from math import pi
@@ -1273,6 +1274,13 @@ class CyclesAOVPass(bpy.types.PropertyGroup):
         default=""
     )
 
+
+class CyclesLightGroup(bpy.types.PropertyGroup):
+    name: StringProperty(name="Name", default="Lightgroup")
+    collection: PointerProperty(name="Collection", type=bpy.types.Collection)
+    include_world: BoolProperty(name="Include World", default=False)
+
+
 class CyclesRenderLayerSettings(bpy.types.PropertyGroup):
 
     pass_debug_bvh_traversed_nodes: BoolProperty(
@@ -1455,6 +1463,15 @@ class CyclesRenderLayerSettings(bpy.types.PropertyGroup):
         min=0
     )
 
+    lightgroups: CollectionProperty(
+        name="Light Groups",
+        type=CyclesLightGroup,
+        )
+    active_lightgroup: IntProperty(
+        name="Active Light Group",
+        default=0,
+        )
+
     @classmethod
     def register(cls):
         bpy.types.ViewLayer.cycles = PointerProperty(
@@ -1630,6 +1647,7 @@ def register():
     bpy.utils.register_class(CyclesDeviceSettings)
     bpy.utils.register_class(CyclesPreferences)
     bpy.utils.register_class(CyclesAOVPass)
+    bpy.utils.register_class(CyclesLightGroup)
     bpy.utils.register_class(CyclesRenderLayerSettings)
     bpy.utils.register_class(CyclesView3DShadingSettings)
 
@@ -1652,5 +1670,6 @@ def unregister():
     bpy.utils.unregister_class(CyclesDeviceSettings)
     bpy.utils.unregister_class(CyclesPreferences)
     bpy.utils.unregister_class(CyclesAOVPass)
+    bpy.utils.unregister_class(CyclesLightGroup)
     bpy.utils.unregister_class(CyclesRenderLayerSettings)
     bpy.utils.unregister_class(CyclesView3DShadingSettings)
